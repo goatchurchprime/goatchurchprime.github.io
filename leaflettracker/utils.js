76006f74d78c60cbb5ceb507cbf99a943b9db751
Julian Todd <julian@goatchurch.org.uk>
2021-04-10 20:17:20 +0100

    make people targets

--- leaflettracker/utils.js
+++ leaflettracker/utils.js
@@ -1,8 +1,8 @@
 
-function decodeHash() 
+function decodeQuery() 
 {
     var hashdict = {};
-    var pairs = (window.location.hash[0] === '#' ? window.location.hash.substr(1) : window.location.hash).split('&');
+    var pairs = (window.location.search[0] === '?' ? window.location.search.substr(1) : window.location.search).split('&');
     for (var i = 0; i < pairs.length; i++) {
         var pair = pairs[i].split('=');
         hashdict[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
